77611ffa97aa03222fc2e55ed58bfb0f2c05e672
Z.H. <galaxyshih@gmail.com>
2026-01-03 00:08:49 +0800

    Init eBPF XDP project

---
+++ ebpf-xdp-program-ebpf/src/main.rs
@@ -0,0 +1,28 @@
+#![no_std]
+#![no_main]
+
+use aya_ebpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
+use aya_log_ebpf::info;
+
+#[xdp]
+pub fn ebpf_xdp_program(ctx: XdpContext) -> u32 {
+    match try_ebpf_xdp_program(ctx) {
+        Ok(ret) => ret,
+        Err(_) => xdp_action::XDP_ABORTED,
+    }
+}
+
+fn try_ebpf_xdp_program(ctx: XdpContext) -> Result<u32, u32> {
+    info!(&ctx, "received a packet");
+    Ok(xdp_action::XDP_PASS)
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[unsafe(link_section = "license")]
+#[unsafe(no_mangle)]
+static LICENSE: [u8; 13] = *b"Dual MIT/GPL\0";
